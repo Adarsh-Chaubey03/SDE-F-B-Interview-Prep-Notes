e8f79b24fae3e17c5f778cd09330475a339b549c
Adarsh Chaubey <adarsh03chaubey@gmail.com>
2026-08-12 08:44:46 +0530

    Add HashMap approach for TwoSum algorithm with time and space complexity notes

--- DSA/TwoSum.java
+++ DSA/TwoSum.java
@@ -1,7 +1,8 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
-
+// two pointer approach - time complexity O(n) and space complexity O(1)
     public static  int[] twoSum(int[] arr, int target){
         int l =0;
         int r =arr.length-1;
@@ -19,6 +20,27 @@ public class TwoSum {
         return new int[]{};
 
     }
+
+
+// HashMap approach - Time: O(n), Space: O(n)
+public static int[] twoSumHashMap(int[] arr, int target) {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < arr.length; i++) {
+
+        int partner = target - arr[i];
+
+        if (map.containsKey(partner)) {
+            return new int[]{arr[i], partner};
+        }
+
+        map.put(arr[i], i);
+    }
+
+    return new int[]{};
+}
+
 
     public static void main(String[] args) {
         int[] arr = {1,2,4,6,7};
