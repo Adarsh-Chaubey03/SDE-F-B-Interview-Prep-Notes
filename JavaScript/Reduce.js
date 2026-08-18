9cd7ac3a79c0f73923566781f8da89eb95f47c57
Adarsh Chaubey <adarsh03chaubey@gmail.com>
2026-08-18 22:51:25 +0530

    Add multiplication example using reduce in Reduce.js

--- JavaScript/Reduce.js
+++ JavaScript/Reduce.js
@@ -3,3 +3,7 @@ const arr = [1,2,3,4,5]
 // reduces array to a single value by applying a function on each element
 const sum = arr.reduce((acc,num)=>acc+num,0);
 console.log(sum);
+
+
+const mul = arr.reduce((acc,num)=>acc*num,1);
+console.log(mul);
