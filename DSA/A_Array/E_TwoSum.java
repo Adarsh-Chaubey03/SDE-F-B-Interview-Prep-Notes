c4adf6cc6bb044ba089c43f1b6af600c29ab935c
Adarsh Chaubey <adarsh03chaubey@gmail.com>
2026-08-26 18:27:46 +0530

    DSA Basic-Array 1

---
+++ DSA/A_Array/E_TwoSum.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+/*
+ * Given an integer array nums and an integer target,
+ * return the indices of the two numbers such that
+ * they add up to target.
+ *
+ * Each input has exactly one solution.
+ * You may not use the same element twice.
+ * Return the answer in any order.
+ */
+
+public class E_TwoSum {
+
+    static int[] twoSum(int[] arr, int target) {
+
+        HashMap<Integer, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            int partner = target - arr[i];
+
+            if (mp.containsKey(partner)) {
+                return new int[]{mp.get(partner), i};
+            }
+
+            mp.put(arr[i], i);
+        }
+
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 4, 5, 3, 4, 3, 8};
+        int target = 8;
+
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
+}
