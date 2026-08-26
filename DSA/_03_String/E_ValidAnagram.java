04e7177f46b7954259a212fcebff3a0420cac019
Adarsh Chaubey <adarsh03chaubey@gmail.com>
2026-08-26 18:50:01 +0530

    String Easy

---
+++ DSA/_03_String/E_ValidAnagram.java
@@ -0,0 +1,49 @@
+package DSA._03_String;
+
+/*
+ * Given two strings s and t, return true if t is an anagram of s,
+ * and false otherwise.
+ *
+ * Input:  s = "anagram", t = "nagaram"
+ * Output: true
+ */
+
+public class E_ValidAnagram {
+
+    static class Solution {
+
+        public static boolean isAnagram(String s, String t) {
+
+            if (s.length() != t.length()) {
+                return false;
+            }
+
+            // Create a frequency array for 26 lowercase English letters
+            int[] freq = new int[26];
+
+            for (int i = 0; i < s.length(); i++) {
+
+                // Convert character to zero-based index
+                freq[s.charAt(i) - 'a']++;
+                freq[t.charAt(i) - 'a']--;
+            }
+
+            // Every frequency should be zero
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] != 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println(Solution.isAnagram(s, t));
+    }
+}
